3a3d4964f3b6896927eb68bf6189b32a25c57e94
charmawhorms <charmawhorms27@gmail.com>
2023-12-22 00:04:39 -0500

    made changes

---
+++ BridgetSandalsClient/Controllers/OrderController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BridgetSandalsClient.Controllers
+{
+    //[Authorize] - the user should be a customer
+    //meaning they have to be signed in before they can 
+    //order a product
+    public class OrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
